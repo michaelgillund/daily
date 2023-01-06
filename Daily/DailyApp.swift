d64a9e12d53c1b56e05e38d0ec03520dec1843ab
Michael Gillund <79627666+michaelgillund@users.noreply.github.com>
2023-01-06 17:04:51 -0600

    Initial Commit

---
+++ Daily/DailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyApp.swift
+//  Daily
+//
+//  Created by Michael Gillund on 1/6/23.
+//
+
+import SwiftUI
+
+@main
+struct DailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
